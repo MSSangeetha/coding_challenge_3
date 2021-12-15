2df7b04fb1ca9546f11976e720466c8fb0dd7be2
Sangeetha <sangeethasanthosh.m@gmail.com>
2021-12-15 16:04:53 -0600

    First push to coding challenge 3

---
+++ src/Coding_Challenge_3.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Coding_Challenge_3 {
+    public static String compare(String str1, String str2){
+        if(str1.length()>str2.length()){
+            return str1;
+        }
+        else{
+            return str2;
+        }
+    }
+
+    public static void main(String[] args) {
+        // write your code here
+        String splitWord;
+
+        Scanner input;
+        input = new Scanner(System.in);
+        System.out.println("Enter the Sentance: ");
+//        splitWord = input.next();
+        splitWord = input.nextLine();
+
+        String[] splitting = splitWord.split(" ");
+
+        System.out.println("The splitted Strings are " + Arrays.toString(splitting));
+        String longest = "";
+        for(int i=0;i<splitting.length;i++){
+
+            longest = compare(longest,splitting[i]);
+
+        }
+        System.out.println("The longest word in the sentance is " +  longest);
+        System.out.println(longest);
+
+
+//        String longest = Arrays.stream(splitting).max(Comparator.comparingInt(String::length)).orElse(null);
+
+
+
+
+    }
+}
+
